88261da2d59982cc8c36ffa633ca00cb3fc43dda
orangerino <47304022+orangerino@users.noreply.github.com>
2020-03-16 20:50:58 +0300

    Update misc.py

--- Project/misc.py
+++ Project/misc.py
@@ -1,8 +1,10 @@
 import time
 import datetime
 import tokens
+import json
 
-def time_converter(seconds): # convert map length to common time format
+
+def time_converter(seconds):   # convert map length to common time format
     hhmmss = [0, 0, 0]
     separator = ':'
     zero = '00'
@@ -51,18 +53,57 @@ def format_time(secs):
     }
     return "{} {}.{}.{}".format(t[3], t[2], months[t[1]], t[-1])
 
-def mods_converter(mods):  # convert mods combination given by user to key format
-    check = len(mods) % 2
-    if check != 0:
-        return 'error'
-    len_by2 = len(mods) // 2
-    final_key = 0
-    for i in range(len_by2):
-        temp_mod = mods[i*2:(i+1)*2]
-        if temp_mod in tokens.mods.values():
-            for mod_keys in tokens.mods.keys():
-                if temp_mod == tokens.mods[mod_keys]:
-                    final_key += int(mod_keys)
-        else:
+def mods_converter(mods, convertion_mode=0):  # convert mods combination given by user to key format
+    if not convertion_mode:
+        if mods == "NOMOD":
+            return 0
+        check = len(mods) % 2
+        if check != 0:
             return 'error'
-    return final_key
+        len_by2 = len(mods) // 2
+        final_key = 0
+        for i in range(len_by2):
+            temp_mod = mods[i*2:(i+1)*2]
+            if temp_mod in tokens.mods.values():
+                for mod_keys in tokens.mods.keys():
+                    if temp_mod == tokens.mods[mod_keys]:
+                        final_key += int(mod_keys)
+            else:
+                return 'error'
+        return final_key
+    if convertion_mode == 1:
+        if mods == 0:
+            return 'Nomod'
+        combination = ''
+        power = 0
+        while mods > 0:
+            while 2 ** power <= mods:
+                power += 1
+            else:
+                power -= 1
+                temp_key = 2 ** power
+                combination = tokens.mods[str(temp_key)] + combination
+                mods -= temp_key
+                power = 0
+        return combination
+
+
+def write_map_to_db(beatmap_id, mods, pp):  # 3459267 HDDT 57.5
+    with open("beatmap_db.json", 'r') as f:
+        data = json.loads(f.read())
+        data[mods][beatmap_id] = pp,
+        data["everything"][beatmap_id] = (pp, mods)
+    with open("beatmap_db.json", 'w') as f:
+        f.write(json.dumps(data))
+
+def write_rate_to_db(beatmap_id, rating, admin_mode=0):
+    beatmap_id = str(beatmap_id)
+    with open("rating_db.json", 'r') as f:
+        data = json.loads(f.read())
+        if beatmap_id not in data.keys():
+            data[beatmap_id] = {"rating": 0, "count": 0}
+        data[beatmap_id]["rating"] = round((data[beatmap_id]["count"] * data[beatmap_id]["rating"] + rating) / (data[beatmap_id]["count"] + 1), 2)
+        if not admin_mode:
+            data[beatmap_id]["count"] += 1
+    with open("rating_db.json", 'w') as f:
+        f.write(json.dumps(data))
