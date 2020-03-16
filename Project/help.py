b600d1e91942bc94e8450d2b7649adfba0d21516
orangerino <47304022+orangerino@users.noreply.github.com>
2020-03-16 20:48:49 +0300

    Create help.py

---
+++ Project/help.py
@@ -0,0 +1,9 @@
+lines = {
+    "1": "1. /np - You give me the map you playing, I give you information about it (how much pp it will give you)",
+    "2": "2. !acc <accuracy> - example: '!acc 97.4' - tells you how many pp you will get for FCing last map you NPed with this acc",
+    "3": "3. !mods <mods> - example: '!mods HDDT' - pp with mods",
+    "4": "4. !stats <player> - example: '!stats Oranger' - tells you player's stats",
+    "5": "5. !rec or !rec <mods> - example: '!rec DT' - recommends you a map to play according to your skill level",
+    "6": "6. !rate <rating> - example: '!rate good' - saves your opinion on last recommended map",
+    "7": "7. !last <player> - example: '!last Rafis' - tells you player's last played beatmap"
+}
